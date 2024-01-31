26e2904837161c0808a7cdbd6941d3633920b6c6
tahayk3 <tahayk3@gmail.com>
2024-01-30 23:29:32 -0600

    Juego de memoria completado

--- src/App.js
+++ src/App.js
@@ -6,21 +6,81 @@ import { images } from './import';
 
 function App() {
   const [cards, setCards] = useState([]);
+  const [firstCard, setFirstCard] = useState({});
+  const [secondCard, setSecondCard] = useState({});
 
+  const [unflippedCards, setUnflippedCards] = useState([]);
+  const [disabledCards, setDisabledCards] = useState([]);
 
   useEffect(()=>{
     shuffleArray(images);
     setCards(images);
   },[]);
 
+  useEffect(()=>{
+    checkForMatch();
+  },[secondCard]);
+
+
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+  }
+
+  const flipCard = (name, number) =>{
+    if(firstCard.name == name && firstCard.number == number){
+      return 0;
+    }
+    if(!firstCard.name){
+      setFirstCard({name,number});
+    }
+    else if(!secondCard.name){
+      setSecondCard({name, number});
+    }
+    return 1;
+  }
+
+
+
+  const checkForMatch= () =>{
+    if(firstCard.name && secondCard.name)
+    {
+      const match = firstCard.name == secondCard.name;
+      match ? disableCards() : unflipCards();
+    }
+  };
+
+  const disableCards = () =>{
+    setDisabledCards([firstCard.number, secondCard.number]);
+    resetCards();
+  };
+
+  const unflipCards = () =>{
+    setUnflippedCards([firstCard.number, secondCard.number]);
+    resetCards();
+  };
+
+  const  resetCards = ()=>{
+    setFirstCard({});
+    setSecondCard({});
+  }
+
   return (
     <div className="App">
       <div className='cards-container'>
         {cards.map((card, index) =>(
-          <Card 
+          <Card
+            key={index} 
             name = {card.player}
             number={index}
-            fronFace={card.img}  
+            fronFace={card.img}
+            flipCard={flipCard}  
+            unflippedCards={unflippedCards}
+            disabledCards={disabledCards}
           />
         ))}   
       </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,35 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import ReactCardFlip from "react-card-flip";
 import backFace from '../images/interrogante.jpg';
 
-const Card = ({name, number, fronFace}) =>{
+const Card = ({name, number, fronFace, flipCard, unflippedCards, disabledCards}) =>{
     const [isFlipped, setIsFlipped] = useState(false);
+    const [hasEvent, setHasEvent] = useState(true);
 
-    const handleClick = (e) =>{
-        setIsFlipped(!isFlipped);
+    useEffect(()=>{
+        if(unflippedCards.includes(number)){
+            setTimeout(() => setIsFlipped(false), 700);
+        }
+    },[unflippedCards]);
+
+    useEffect(()=>{
+        if(disabledCards.includes(number)){
+            setHasEvent(false);
+        }
+    },[disabledCards]);
+
+    const handleClick = e =>{
+        const value = flipCard(name,number);
+        if(value !== 0){
+            setIsFlipped(!isFlipped);
+        }
     }
 
     return(
         <div className="card">
             <ReactCardFlip isFlipped={isFlipped}>
-                <img className="card-image" src={backFace} onClick={handleClick}/>
-                <img className="card-image" src={fronFace}/>
+                <img className="card-image" src={backFace} onClick={hasEvent? handleClick: null}/>
+                <img className="card-image" src={fronFace} onClick={hasEvent? handleClick: null}/>
             </ReactCardFlip>
         </div>
     );
